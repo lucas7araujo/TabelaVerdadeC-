cb2578e7fd9910824fd2fefb773fa2a49c7f6351
Lucas Gabriel Adelino Araújo <lucasgabrielz2208@gmail.com>
2025-05-10 12:01:34 -0300

    Adição Funções - Armazenamento, Conjunção e Disjunção

--- Program.cs
+++ Program.cs
@@ -11,16 +11,42 @@ class Program
         do
         {
             Console.Write(mensagem);
-            entrada = char.ToUpper(Console.ReadKey().KeyChar); 
-            Console.WriteLine(); 
+            entrada = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
 
-        } while (entrada != 'V' && entrada != 'F'); 
+        } while (entrada != 'V' && entrada != 'F');
 
-        return entrada == 'V'; 
+        return entrada == 'V';
     }
+
+    static bool Conjunção(bool p, bool q)
+    {
+        return p && q;
+    }
+
+    static bool Disjunção(bool p, bool q)
+    {
+        return p || q;
+    }
+
     static void Main()
     {
-        bool p = LerValor("Digite V ou F: ");
+        bool p = LerValor("\nDigite um valor para P (V ou F): ");
         Console.WriteLine($"O valor de P é: {p}");
+        bool q = LerValor("\nDigte um valor para Q (V ou F): ");
+        Console.WriteLine($"O valor de P é: {q}");
+
+        Console.Write("\nO que deseja verificar? \n1 - Conjunção; \n2 - Disjunção\n \nOpção Escolhida -> ");
+        int opcao = int.Parse(Console.ReadLine());
+
+        if (opcao == 1)
+        {
+            Console.WriteLine($"P (AND) Q = {Conjunção(p, q)}");
+        }
+
+        else if (opcao == 2){
+            Console.WriteLine($"P (OR) Q = {Disjunção(p, q)}");
+        }
+
     }
 }
