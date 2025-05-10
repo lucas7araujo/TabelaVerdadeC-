c8295391c5eea34049f40864db35c7b700567f11
Lucas Gabriel Adelino Araújo <lucasgabrielz2208@gmail.com>
2025-05-10 11:29:17 -0300

    Criação Estrutura Inicial - Verificação valor Booleano

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿// Projeto Crição de tabela verdade
 
 using System;
+class Program
+{
 
-class Program{
-    static void Main(){
+    static bool LerValor(string mensagem)
+    {
+        char entrada;
 
-        
+        do
+        {
+            Console.Write(mensagem);
+            entrada = char.ToUpper(Console.ReadKey().KeyChar); 
+            Console.WriteLine(); 
 
+        } while (entrada != 'V' && entrada != 'F'); 
 
-
+        return entrada == 'V'; 
+    }
+    static void Main()
+    {
+        bool p = LerValor("Digite V ou F: ");
+        Console.WriteLine($"O valor de P é: {p}");
     }
 }
